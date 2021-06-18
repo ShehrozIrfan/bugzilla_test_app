4c138cb40c9a4617848f7d609896a1092bd66520
shehrozirfan <shehrozirfan89@gmail.com>
2021-06-18 16:33:10 +0500

    Sign Up done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,26 @@
 class UsersController < ApplicationController
   def new
+    @user = User.new
   end
+
+  def create
+    #render plain: params[:user].inspect
+    @user = User.new(user_params)
+    if @user.save
+      flash[:success] = "Welcome to Bugzilla App!"
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  def show
+
+  end
+
+
+  private
+    def user_params
+      params.require(:user).permit(:name,:email,:password,:password_confirmation)
+    end
 end
